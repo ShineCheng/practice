ae0fd4da04db8570f584e597c68e4a8ca35cd637
ShineCheng <afireflytw@gmail.com>
2019-05-03 13:39:22 +0800

    vue router practice

---
+++ vue-router/data.js
@@ -0,0 +1,10 @@
+const data = [
+    {
+        title: "Startups Took Over Earth and Now They’re Headed to Space — With Creepy Pepsi Ads in Tow",
+        article: "Last November, Reuters released a bombshell report detailing the location of 39 Muslim “reeducation” camps in China’s Xinjiang province. Now widely understood as detention centers perpetuating serious human rights abuses, these camps had expanded in secret for years. But for all its restrictions, Beijing couldn’t hide them from a set of eyes hovering in Lower Earth Orbit (LEO)."
+    },
+    {
+        title: "Sri Lanka’s Decision to Censor Social Platforms Is Indefensible",
+        article: "The best advice I’ve ever got about reading came from a secretive movie producer and talent manager who’d sold more than 100 million albums and done more than $1B in box office returns. He said to me one day, “Ryan, it’s not enough that you read a lot. To do great things, you have to read to lead.  What he meant was that in an age where almost nobody reads, you can be forgiven for thinking that the simple act of picking up a book is revolutionary. It may be, but it’s not enough. Reading to lead means pushing yourself–reading books “above your level.”"
+    }
+]
